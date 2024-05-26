a6cae8b37f0b6b8eb1232e115ef84f6c93ce81cd
Amraz Rafeeque <amrazrafeek2020@gmail.com>
2024-05-26 15:40:35 +0530

    admin side bug fixed

--- ecom/admin_app/urls.py
+++ ecom/admin_app/urls.py
@@ -66,8 +66,11 @@ urlpatterns = [
     ),
     path("view_brand/", views.view_brand, name="view_brand"),
     path("add_brand/", views.add_brand, name="add_brand"),
+    path("delete_brand/<int:brand_id>/", views.delete_brand, name="delete_brand"),
+    path("edit_brand/<int:brand_id>/", views.edit_brand, name="edit_brand"),
     path("banner/", views.banner, name="banner"),
     path("add_banner/", views.add_banner, name="add_banner"),
     path("admin/edit-banner/<int:banner_id>/", views.edit_banner, name="edit_banner"),
     path("delete-banner/<int:banner_id>/", views.delete_banner, name="delete_banner"),
+    
 ]
